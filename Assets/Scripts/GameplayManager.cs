6c2e367d8fecccb46c89241b017c21f930c665ee
Robert Lafferty <rclafferty@ksu.edu>
2019-02-08 23:39:27 -0600

    Updated GameplayManager, Added PlayerController

--- Assets/Scripts/GameplayManager.cs
+++ Assets/Scripts/GameplayManager.cs
@@ -45,17 +45,50 @@ public class GameplayManager : MonoBehaviour
         humans.Add(mainPlayer);
 
         // Find all humans -- NOT player
-        GameObject[] h = GameObject.FindGameObjectsWithTag("Human");
+        /* GameObject[] h = GameObject.FindGameObjectsWithTag("Human");
         foreach (GameObject thisHuman in h)
         {
             humans.Add(thisHuman);
-        }
+        }*/
 
         // Find all enemies
-        GameObject[] e = GameObject.FindGameObjectsWithTag("Enemy");
+        /* GameObject[] e = GameObject.FindGameObjectsWithTag("Enemy");
         foreach (GameObject thisEnemy in e)
         {
             enemies.Add(thisEnemy);
+        }*/
+    }
+
+    // Getter/Setter methods
+    public float DeadValue
+    {
+        get
+        {
+            return deadValue;
+        }
+    }
+
+    public GameObject MainPlayer
+    {
+        get
+        {
+            return mainPlayer;
+        }
+    }
+
+    public ArrayList Enemies
+    {
+        get
+        {
+            return enemies;
+        }
+    }
+
+    public ArrayList Humans
+    {
+        get
+        {
+            return humans;
         }
     }
 }
