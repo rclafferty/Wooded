b014f4ff05039311dce832a5da5d1a62b7303bd5
Robert Lafferty <rclafferty@ksu.edu>
2019-02-08 23:22:42 -0600

    Added Inheritance in C#, plus Human/Enemy/Character classes

---
+++ Assets/Scripts/Classes/Character.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Classes
+{
+    class Character
+    {
+        private GameObject sceneObject;
+        private string name;
+
+        public Character(GameObject g)
+        {
+            sceneObject = g;
+            name = g.name;
+        }
+
+        public GameObject SceneObject
+        {
+            get
+            {
+                return sceneObject;
+            }
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+    }
+}
