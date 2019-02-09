680909531a1b288e61164c952964a682223eb605
Robert Lafferty <rclafferty@ksu.edu>
2019-02-08 23:49:00 -0600

    Re-added Asset Files

---
+++ Assets/Scripts/Classes/Character.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Classes
+{
+    class Character
+    {
+        private GameObject sceneObject;
+        private string name;
+
+        public Character(GameObject g)
+        {
+            sceneObject = g;
+            name = g.name;
+        }
+
+        public GameObject SceneObject
+        {
+            get
+            {
+                return sceneObject;
+            }
+            /* set
+            {
+                sceneObject = value;
+            } */
+        }
+
+        public string Name
+        {
+            get
+            {
+                return name;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/Classes/Enemy.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using UnityEngine;
+
+namespace Assets.Scripts.Classes
+{
+    class Enemy : Character
+    {
+        public Enemy(GameObject g) : base(g)
+        {
+
+        }
+    }
+}
